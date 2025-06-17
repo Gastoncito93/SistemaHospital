f38f2d1079bb314d44deb235228d15a20ebd94ce
Gastoncito93 <gastonoscarsosa@gmail.com>
2025-06-17 10:26:38 -0300

    Reparacion DB

--- config/db.js
+++ config/db.js
@@ -3,17 +3,19 @@ const mysql = require('mysql2/promise');
 let connection;
 
 async function conectar() {
-    if (!connection) {
-        connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            port: process.env.DB_PORT
-        });
-        console.log('Conexión correcta.');
-    }
-    return connection;
+  // Si no existe o está cerrada (ej: destroyed, ended), reconectar
+  if (!connection || connection.connection._closing || connection.connection._fatalError) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT
+    });
+    console.log('Conexión correcta.');
+  }
+
+  return connection;
 }
 
 module.exports = conectar;
